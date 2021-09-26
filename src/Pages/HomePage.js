f182256f872626f42cd507e8f180dde1ecf3332d
Saurav1999 <singhsaurav1999@gmail.com>
2021-09-26 14:43:33 +0530

    Initial Commit

---
+++ src/Pages/HomePage.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import {faFacebook} from '@fortawesome/free-brands-svg-icons';
+import {faGithub} from '@fortawesome/free-brands-svg-icons';
+import {faYoutube} from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {Link} from 'react-router-dom';
+
+function HomePage() {
+    return (
+        <div className="HomePage">
+            <header className="hero">
+                <h1 className="hero-text">
+                    Hi, I am 
+                    <span> Saurav.</span>
+                </h1>
+                <p className="h-sub-text">
+                Web developer with a passion for thoughtful UI. I develop elegant and responsive frontend with optimised and modern backend. I actively seek out new technologies and stay up-to-date on industry trends and advancements.
+                </p>
+                <div className="icons">
+                    <Link to="test" className="icon-holder">
+                        <FontAwesomeIcon icon={faFacebook} className="icon fb" />
+                    </Link>
+                    <Link to="test" className="icon-holder">
+                        <FontAwesomeIcon icon={faGithub} className="icon gh" />
+                    </Link>
+                    <Link to="test" className="icon-holder">
+                        <FontAwesomeIcon icon={faYoutube} className="icon yt"/>
+                    </Link>
+                </div>
+            </header>
+        </div>
+    )
+}
+
+export default HomePage;

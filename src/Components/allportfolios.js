fdcd4d3556f9208f47802040009bfb96e815df89
Saurav1999 <singhsaurav1999@gmail.com>
2021-10-09 21:32:06 +0530

    Added new project

--- src/Components/allportfolios.js
+++ src/Components/allportfolios.js
@@ -1,5 +1,5 @@
 import port1 from '../img/portImages/port1.PNG';
-// import port2 from '../img/portImages/port1.jpg';
+import port2 from '../img/portImages/port2.png';
 // import port3 from '../img/portImages/port2.jpg';
 // import port4 from '../img/portImages/port3.jpg';
 // import port6 from '../img/portImages/port5.jpg';
@@ -17,6 +17,28 @@ export const projectList = [
         image: port1,
         title: 'myAadhaar UIDAI',
         description:"An application built for 1.3+ billion Aadhaar users in India to provide them with all possible aadhaar services online."
+    },
+    {
+        id: 1,
+        category: 'React Js',
+        link1: 'https://github.com/Saurav1999/Portfolio',
+        link2: 'https://www.saurav1999.tech/',
+        label1: 'Code',
+        label2: 'Live',
+        image: port2,
+        title: 'Portfolio Website',
+        description:"A portfolio website just like the one you have just visited but with different look and feel."
+    },
+    {
+        id: 2,
+        category: 'React Js',
+        link1: 'https://github.com/Saurav1999/Portfolio',
+        link2: 'https://www.saurav1999.tech/',
+        label1: 'Code',
+        label2: 'Live',
+        image: port2,
+        title: 'Portfolio Website',
+        description:"A portfolio website just like the one you have just visited but with different look and feel."
     },
   
 ]

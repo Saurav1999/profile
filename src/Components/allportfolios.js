190a2cf0433fbb90a034de6c09167fe2aef09ac4
Saurav1999 <singhsaurav1999@gmail.com>
2021-10-09 23:53:12 +0530

    Added mini projects.

--- src/Components/allportfolios.js
+++ src/Components/allportfolios.js
@@ -1,9 +1,14 @@
 import port1 from '../img/portImages/port1.PNG';
 import port2 from '../img/portImages/port2.png';
-// import port3 from '../img/portImages/port2.jpg';
-// import port4 from '../img/portImages/port3.jpg';
-// import port6 from '../img/portImages/port5.jpg';
-// import port7 from '../img/portImages/port6.jpg';
+import port3 from '../img/portImages/port3.png';
+
+import mini1 from '../img/miniProjects/mini1.png';
+import mini2 from '../img/miniProjects/mini2.png';
+import mini3 from '../img/miniProjects/mini3.png';
+import mini4 from '../img/miniProjects/mini4.png';
+import mini5 from '../img/miniProjects/mini5.png';
+import mini6 from '../img/miniProjects/mini6.png';
+
 
 
 export const projectList = [
@@ -19,17 +24,6 @@ export const projectList = [
         description:"An application built for 1.3+ billion Aadhaar users in India to provide them with all possible aadhaar services online."
     },
     {
-        id: 1,
-        category: 'React Js',
-        link1: 'https://github.com/Saurav1999/Portfolio',
-        link2: 'https://www.saurav1999.tech/',
-        label1: 'Code',
-        label2: 'Live',
-        image: port2,
-        title: 'Portfolio Website',
-        description:"A portfolio website just like the one you have just visited but with different look and feel."
-    },
-    {
         id: 2,
         category: 'React Js',
         link1: 'https://github.com/Saurav1999/Portfolio',
@@ -37,8 +31,19 @@ export const projectList = [
         label1: 'Code',
         label2: 'Live',
         image: port2,
-        title: 'Portfolio Website',
-        description:"A portfolio website just like the one you have just visited but with different look and feel."
+        title: 'Portfolio Website I',
+        description:"A portfolio website just like the one you are at now, but with different look and feel."
+    },
+    {
+        id: 3,
+        category: 'Next Js',
+        link1: '',
+        link2: 'https://portfolio-psi-livid.vercel.app/',
+        label1: 'Code',
+        label2: 'Live',
+        image: port3,
+        title: 'Portfolio Website II',
+        description:"A portfolio website just like the one you are at now, but with different look and feel."
     },
   
 ]
@@ -48,10 +53,65 @@ export const miniProjectsList = [
         id: 1,
         category: 'Vanilla JS',
         // link1: '',
+        // link2: 'https://myaadhaar.uidai.gov.in/',
+        label1: 'Code',
+        label2: 'Live',
+        image: mini1,
+        title: 'myAadhaar UIDAI',
+        description:"An application built for 1.3+ billion Aadhaar users in India to provide them with all possible aadhaar services online."
+    },
+    {
+        id: 2,
+        category: 'Vanilla JS',
+        // link1: '',
         link2: 'https://myaadhaar.uidai.gov.in/',
         label1: 'Code',
         label2: 'Live',
-        image: port1,
+        image: mini2,
+        title: 'myAadhaar UIDAI',
+        description:"An application built for 1.3+ billion Aadhaar users in India to provide them with all possible aadhaar services online."
+    },
+    {
+        id: 3,
+        category: 'Vanilla JS',
+        // link1: '',
+        link2: 'https://myaadhaar.uidai.gov.in/',
+        label1: 'Code',
+        label2: 'Live',
+        image: mini3,
+        title: 'myAadhaar UIDAI',
+        description:"An application built for 1.3+ billion Aadhaar users in India to provide them with all possible aadhaar services online."
+    },
+    {
+        id: 4,
+        category: 'Vanilla JS',
+        // link1: '',
+        link2: 'https://myaadhaar.uidai.gov.in/',
+        label1: 'Code',
+        label2: 'Live',
+        image: mini4,
+        title: 'myAadhaar UIDAI',
+        description:"An application built for 1.3+ billion Aadhaar users in India to provide them with all possible aadhaar services online."
+    },
+    {
+        id: 5,
+        category: 'Vanilla JS',
+        // link1: '',
+        link2: 'https://myaadhaar.uidai.gov.in/',
+        label1: 'Code',
+        label2: 'Live',
+        image: mini5,
+        title: 'myAadhaar UIDAI',
+        description:"An application built for 1.3+ billion Aadhaar users in India to provide them with all possible aadhaar services online."
+    },
+    {
+        id: 6,
+        category: 'Vanilla JS',
+        // link1: '',
+        link2: 'https://myaadhaar.uidai.gov.in/',
+        label1: 'Code',
+        label2: 'Live',
+        image: mini6,
         title: 'myAadhaar UIDAI',
         description:"An application built for 1.3+ billion Aadhaar users in India to provide them with all possible aadhaar services online."
     },

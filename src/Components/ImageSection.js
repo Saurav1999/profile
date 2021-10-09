33e31e1b35f06a39933dc1bbffb6300cbcdfab82
Saurav1999 <singhsaurav1999@gmail.com>
2021-10-10 02:45:22 +0530

    Changed Resume Link

--- src/Components/ImageSection.js
+++ src/Components/ImageSection.js
@@ -30,7 +30,7 @@ function ImageSection() {
             <p>: Whitefield, Bengaluru, Karnataka, India</p>
           </div>
         </div>
-        <a href="https://drive.google.com/file/d/1_a4tu77TeeWz3vrmbVV9IYmxNqtKyGIa/view?usp=sharing">
+        <a href="https://drive.google.com/file/d/1KAD59WOgHxDav2M5auGTDwlgo9EGy8IR/view?usp=sharing">
           <button className="btn">Resume</button>
         </a>
       </div>
